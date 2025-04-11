899ef4acfdd3a3e66a7a2cea65b01a48f5eb9e38
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-04-11 17:47:29 +0100

    stcrap

--- balrog/environments/pogs/pogs_env.py
+++ balrog/environments/pogs/pogs_env.py
@@ -11,7 +11,7 @@ def make_pogs_env(env_name, task, config, render_mode: Optional[str] = None):
         num_nodes=config.envs.pogs_kwargs.num_nodes,
         k_nearest=config.envs.pogs_kwargs.k_nearest,
         min_backtracks=config.envs.pogs_kwargs.min_backtracks,
-        max_episode_steps=config.envs.pogs_kwargs.max_episode_steps,
+        max_steps=config.envs.pogs_kwargs.max_steps,
     )
     env = gym.make(task, **pogs_kwargs, render_mode=render_mode)
     env = POGSWrapper(env)
