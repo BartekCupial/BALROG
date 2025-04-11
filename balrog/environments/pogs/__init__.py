36cadeaed6978f729244ebbe6a1434a81efd3302
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-04-11 14:24:08 +0100

    update pogs to gymnasium

--- balrog/environments/pogs/__init__.py
+++ balrog/environments/pogs/__init__.py
@@ -8,14 +8,14 @@ def get_instruction_prompt(env, task):
 You are an AI agent designed to navigate the Partially Observable Graph Search (POGS) environment. Your goal is to find and reach a target node in a partially observable graph by exploring the graph structure.
 
 The following are the only valid actions you can take in the game:
-{list(range(env.num_nodes))}
+{list(range(env.unwrapped.num_nodes))}
 
 In a moment I will present you with an observation containing:
 - Adjacency list of each visible
 - Your current node position
 - The target node you need to reach
 
-The graph has {env.num_nodes} nodes and is partially observable, meaning you can only see connections within a k-nearest neighbor radius of your current position. In this episode k={env.k_nearest}, graph={env.undirected}.
+The graph has {env.unwrapped.num_nodes} nodes and is partially observable, meaning you can only see connections within a k-nearest neighbor radius of your current position. In this episode k={env.unwrapped.k_nearest}, graph={env.unwrapped.undirected}.
 
 Your action should be a single integer representing the label of the node you want to travel to. This node must be directly connected to your current node.
 
